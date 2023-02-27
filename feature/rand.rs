73d9e5866f42180017ed418b831b8a0a43fe9b2a
Oleh Martsokha <o.martsokha@gmail.com>
2023-02-27 06:15:23 +0100

    oops

--- feature/rand.rs
+++ feature/rand.rs
@@ -2,37 +2,11 @@ use rand::distributions::{Distribution, Standard};
 use rand::Rng;
 
 use crate::CalendarDuration;
-use crate::InComplete;
-use crate::{InDate, InOffsetDateTime, InPrimitiveDateTime, InTime};
 
 impl Distribution<CalendarDuration> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> CalendarDuration {
         let min = CalendarDuration::MIN.whole_months();
         let max = CalendarDuration::MAX.whole_months();
         CalendarDuration::months(rng.gen_range(min..=max))
-    }
-}
-
-impl Distribution<InDate> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> InDate {
-        InDate::from_complete(Self.sample(rng))
-    }
-}
-
-impl Distribution<InTime> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> InTime {
-        InTime::from_complete(Self.sample(rng))
-    }
-}
-
-impl Distribution<InPrimitiveDateTime> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> InPrimitiveDateTime {
-        InPrimitiveDateTime::from_complete(Self.sample(rng))
-    }
-}
-
-impl Distribution<InOffsetDateTime> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> InOffsetDateTime {
-        InOffsetDateTime::from_complete(Self.sample(rng))
     }
 }
